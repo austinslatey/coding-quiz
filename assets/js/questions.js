51caeccd382be380cb0ddb3a038db68b8ab5fb27
Mr WhiteKidd <austinslater58@gmail.com>
2022-05-08 04:59:43 -0500

    reworked html, added array in questions.js with 4 questions

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var quizQuestions = [
+    {
+        title: "What is the use of the <noscript> tag in Javascript?",
+        choices: ["Clears Cache", "Contents are displayed by non Javascript browsers.", "Inserts a function into an array"], 
+        answer: "Contents are displayed by non Javascript browsers."
+    },
+    {   
+        title: "Which of the following is not JavaScript Data Types?",
+        choices: ["Undefined", "Float", "Boolean"],
+        answer: "Float" 
+    },
+    {
+        title: "Which company developed JavaScript",
+        choices: ["Bell Labs", "Microsoft", "Netscape"],
+        answer: "Netscape" 
+    },
+    {
+        title: "What are the advantages of JavaScript?",
+        choices: ["Dreamweaver", "FrontPage", "Both of these"],
+        answer: "FrontPage" 
+    }
+];
